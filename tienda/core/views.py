755b05da8dc4b33247b1e29e5ef22b13031512d4
JesusTapia-dev <jtapiag@uni.pe>
2023-07-19 23:11:28 -0500

    New Product-Shows last three

--- tienda/core/views.py
+++ tienda/core/views.py
@@ -4,9 +4,12 @@ from item.models import Item, Category
 def index(request):
     items=Item.objects.filter(is_sold=False)[0:6]
     categories=Category.objects.all()
+    new_items=Item.objects.filter(is_sold=False).order_by("-created_at")[0:3]
+
     return render(request, 'core/index.html',{
         'categories':categories,
         'items':items, 
+        'new_items':new_items,
     })
 def contact(request):
     items=Item.objects.filter(is_sold=False)
